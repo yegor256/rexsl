a8a226d7110d7142574841f76af5b7477d34cc90
Yegor Bugayenko <yegor@tpc2.com>
2011-09-15 08:54:14 +0000

    refs #3 - preliminary version of TestClient introduced

---
+++ rexsl/rexsl-test/src/main/java/com/rexsl/test/TestClient.java
@@ -0,0 +1,185 @@
+/**
+ * Copyright (c) 2011, ReXSL.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: 1) Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer. 2) Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3) Neither the name of the ReXSL.com nor
+ * the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.rexsl.test;
+
+import com.rexsl.test.client.Extender;
+import com.rexsl.test.client.HeaderExtender;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+/**
+ * Test HTTP client.
+ *
+ * @author Yegor Bugayenko (yegor@rexsl.com)
+ * @version $Id$
+ */
+public final class TestClient {
+
+    /**
+     * Document root.
+     */
+    private final URI home;
+
+    /**
+     * List of HTTP request/response extenders.
+     */
+    private final List<Extender> extenders = new ArrayList<Extender>();
+
+    /**
+     * HTTP response.
+     */
+    private HttpResponse response;
+
+    /**
+     * Public ctor.
+     * @param uri Home of the server
+     */
+    public TestClient(final URI uri) {
+        this.home = uri;
+    }
+
+    /**
+     * Set request header.
+     * @param name Header name
+     * @param value Value of the header to set
+     * @return This object
+     */
+    public TestClient header(final String name, final String value) {
+        this.extenders.add(new HeaderExtender(name, value));
+        return this;
+    }
+
+    /**
+     * Execute GET request.
+     * @param path The URL
+     * @return This object
+     * @throws Exception If something goes wrong
+     */
+    public TestClient get(final String path) throws Exception {
+        this.response = this.execute(new HttpGet(this.uri(path)));
+        return this;
+    }
+
+    /**
+     * Execute POST request.
+     * @param path The URL
+     * @return This object
+     * @throws Exception If something goes wrong
+     */
+    public TestClient post(final String path) throws Exception {
+        this.response = this.execute(new HttpPost(this.uri(path)));
+        return this;
+    }
+
+    /**
+     * Execute PUT request.
+     * @param path The URL
+     * @return This object
+     * @throws Exception If something goes wrong
+     */
+    public TestClient put(final String path) throws Exception {
+        this.response = this.execute(new HttpPut(this.uri(path)));
+        return this;
+    }
+
+    /**
+     * Get body as a string.
+     * @return The body
+     * @throws java.io.IOException If some IO problem inside
+     */
+    public String getBody() throws java.io.IOException {
+        return IOUtils.toString(this.response.getEntity().getContent());
+    }
+
+    /**
+     * Get status of the response as a number.
+     * @return The status
+     */
+    public Integer getStatus() {
+        return this.response.getStatusLine().getStatusCode();
+    }
+
+    /**
+     * Get status line of the response.
+     * @return The status line
+     */
+    public String getStatusLine() {
+        return String.format(
+            "%s %s %s",
+            this.response.getStatusLine().getProtocolVersion(),
+            this.response.getStatusLine().getReasonPhrase(),
+            this.response.getStatusLine().getStatusCode()
+        );
+    }
+
+    /**
+     * Build URI to request.
+     * @param path The path to request
+     * @return The URI
+     * @throws java.net.URISyntaxException If there is some problem
+     */
+    private URI uri(final String path) throws java.net.URISyntaxException {
+        return new URI(
+            String.format(
+                "%s://%s:%d%s",
+                this.home.getScheme(),
+                this.home.getHost(),
+                this.home.getPort(),
+                path
+            )
+        );
+    }
+
+    /**
+     * Execute request and return response.
+     * @param req The request
+     * @return The response
+     * @throws java.io.IOException If some IO problem
+     */
+    private HttpResponse execute(final HttpRequest req)
+        throws java.io.IOException {
+        for (Extender extender : this.extenders) {
+            extender.extend(req);
+        }
+        final HttpClient client = new DefaultHttpClient();
+        return client.execute((HttpUriRequest) req);
+    }
+
+}
---
+++ rexsl/rexsl-test/src/test/java/com/rexsl/test/TestClientTest.java
@@ -0,0 +1,111 @@
+/**
+ * Copyright (c) 2011, ReXSL.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: 1) Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer. 2) Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3) Neither the name of the ReXSL.com nor
+ * the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.rexsl.test;
+
+import com.sun.grizzly.http.embed.GrizzlyWebServer;
+import com.sun.grizzly.http.servlet.ServletAdapter;
+import com.sun.grizzly.tcp.http11.GrizzlyAdapter;
+import com.sun.grizzly.tcp.http11.GrizzlyRequest;
+import com.sun.grizzly.tcp.http11.GrizzlyResponse;
+import java.net.URI;
+import org.apache.http.HttpStatus;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+/**
+ * Test HTTP client.
+ *
+ * @author Yegor Bugayenko (yegor@rexsl.com)
+ * @version $Id$
+ */
+public final class TestClientTest {
+
+    /**
+     * Grizzly container, used for tests.
+     */
+    private GrizzlyWebServer gws;
+
+    /**
+     * Where this Grizzly container is hosted.
+     */
+    private URI home;
+
+    /**
+     * Start Servlet container.
+     * @throws Exception If something goes wrong inside
+     */
+    @Before
+    public void startContainer() throws Exception {
+        final Integer port =
+            Integer.valueOf(System.getProperty("grizzly.port"));
+        this.home = new URI(String.format("http://localhost:%d/", port));
+        this.gws = new GrizzlyWebServer(port);
+        this.gws.addGrizzlyAdapter(
+            new GrizzlyAdapter() {
+                public void service(final GrizzlyRequest request,
+                    final GrizzlyResponse response) {
+                    try {
+                        response.getWriter().println("works fine!");
+                    } catch (java.io.IOException ex) {
+                        throw new IllegalStateException(ex);
+                    }
+                }
+            }
+        );
+        this.gws.start();
+    }
+
+    /**
+     * Stop Servlet container.
+     * @throws Exception If something goes wrong inside
+     */
+    @After
+    public void stopContainer() throws Exception {
+        this.gws.stop();
+    }
+
+    /**
+     * Test simple HTTP interaction.
+     * @throws Exception If something goes wrong inside
+     */
+    @Test
+    public void testHTTPRequests() throws Exception {
+        final TestClient client = new TestClient(this.home)
+            .header("Accept", "application/json")
+            .header("User-agent", "Some Text")
+            .get("/");
+        assertThat(client.getBody(), containsString("works"));
+        assertThat(client.getStatus(), equalTo(HttpStatus.SC_OK));
+    }
+
+}
