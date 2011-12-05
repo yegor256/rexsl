c32fc210a46afe0c7a72195454c1cc1e7a51e167
Dmitry Bashkin <dmitry.bashkin@gmail.com>
2011-12-05 20:02:42 +0000

    refs #36 - done

---
+++ rexsl/rexsl-maven-plugin/src/main/java/com/rexsl/maven/packers/YuiCompressorErrorReporter.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright (c) 2011, ReXSL.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: 1) Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer. 2) Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3) Neither the name of the ReXSL.com nor
+ * the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.rexsl.maven.packers;
+
+import com.ymock.util.Logger;
+import org.mozilla.javascript.ErrorReporter;
+import org.mozilla.javascript.EvaluatorException;
+
+/**
+ * Error reporter of Javascript compressor.
+ *
+ * @author Dmitry Bashkin (dmitry.bashkin@rexsl.com)
+ * @version $Id: YuiCompressorErrorReporter.java 370 2011-11-27 16:36:01Z guard $
+ */
+public class YuiCompressorErrorReporter implements ErrorReporter {
+    /**
+     * {@inheritDoc}
+     */
+    public void warning(
+        final String message,
+        final String sourceName,
+        final int line,
+        final String lineSource,
+        final int lineOffset
+        ) {
+        if (line < 0) {
+            Logger.warn(this, message);
+        } else {
+            Logger.warn(this, line + ':' + lineOffset + ':' + message);
+        }
+    }
+    /**
+     * {@inheritDoc}
+     */
+    public void error(
+        final String message,
+        final String sourceName,
+        final int line,
+        final String lineSource,
+        final int lineOffset
+        ) {
+        if (line < 0) {
+            Logger.error(this, message);
+        } else {
+            Logger.error(this, line + ':' + lineOffset + ':' + message);
+        }
+    }
+    /**
+     * {@inheritDoc}
+     */
+    public EvaluatorException runtimeError(
+        final String message,
+        final String sourceName,
+        final int line,
+        final String lineSource,
+        final int lineOffset
+        ) {
+        error(message, sourceName, line, lineSource, lineOffset);
+        return new EvaluatorException(message);
+    }
+}
