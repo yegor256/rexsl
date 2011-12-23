2a1958838809a7279e0efdf19a00b4e796669130
Yegor Bugayenko <yegor@tpc2.com>
2011-12-23 21:15:03 +0000

    refs #92 - TestClientMocker introduced

---
+++ rexsl/rexsl-test/src/mock/java/com/rexsl/test/TestResponseMocker.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) 2011, ReXSL.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: 1) Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer. 2) Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3) Neither the name of the ReXSL.com nor
+ * the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.rexsl.test;
+
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
+import java.net.HttpURLConnection;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ws.rs.core.MultivaluedMap;
+import org.mockito.Mockito;
+
+/**
+ * Mocker of {@link TestResponse}.
+ * @author Yegor Bugayenko (yegor@netbout.com)
+ * @version $Id$
+ */
+public final class TestResponseMocker {
+
+    /**
+     * Mock.
+     */
+    private final transient TestResponse response =
+        Mockito.mock(TestResponse.class);
+
+    /**
+     * Public ctor.
+     */
+    public TestResponseMocker() {
+        this.withBody("");
+    }
+
+    /**
+     * Return this body.
+     * @param body The body
+     * @return This object
+     */
+    public TestResponseMocker withBody(final String body) {
+        Mockito.doReturn(body).when(this.response).getBody();
+        return this;
+    }
+
+    /**
+     * Mock it.
+     * @return Mocked class
+     */
+    public TestResponse mock() {
+        return this.response;
+    }
+
+}
