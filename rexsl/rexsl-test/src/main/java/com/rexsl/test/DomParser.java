8d30584c16f1f082290388c8b5f265775d1f7bc2
Yegor Bugayenko <yegor@tpc2.com>
2012-04-29 09:30:03 +0000

    refs #391 - fixed the defect explained in #335

--- rexsl/rexsl-test/src/main/java/com/rexsl/test/DomParser.java
+++ rexsl/rexsl-test/src/main/java/com/rexsl/test/DomParser.java
@@ -52,7 +52,17 @@ final class DomParser {
 
     /**
      * Public ctor.
+     *
+     * <p>An {@link IllegalArgumentException} may be thrown if the parameter
+     * passed is not in XML format. It doesn't perform a strict validation
+     * and is not guaranteed that an exception will be thrown whenever
+     * the parameter is not XML.
+     *
      * @param txt The XML in text
+     * @todo #391 Would be great to improve the validation. We should check the
+     *  incoming text for XML compliance before it's checked in
+     *  DocumentBuilderFactory. We can't guarantee a proper validation, but
+     *  something better than current implementation should be introduced
      */
     public DomParser(final String txt) {
         if (txt == null) {
