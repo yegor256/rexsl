cae671afab858bd943d6306cccc7652b6cf920c9
Yegor Bugayenko <yegor@tpc2.com>
2012-10-28 15:44:26 +0000

    refs #596 - `CookieBuilder` moved to rexsl-page

--- rexsl/rexsl-page/src/main/java/com/rexsl/page/CookieBuilder.java
+++ rexsl/rexsl-page/src/main/java/com/rexsl/page/CookieBuilder.java
@@ -27,7 +27,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package com.rexsl.misc;
+package com.rexsl.page;
 
 import java.net.URI;
 import java.util.Calendar;
