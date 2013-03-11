5d1c3a882ca438ee2ebfca67cef0af47f3da05de
Yegor Bugayenko <yegor@tpc2.com>
2013-03-11 16:15:17 +0000

    refs #628 - archetype fixed for insets

--- rexsl-maven-archetype/src/main/resources/archetype-resources/src/main/java/BaseRs.java
+++ rexsl-maven-archetype/src/main/resources/archetype-resources/src/main/java/BaseRs.java
@@ -4,8 +4,17 @@
  */
 package ${package};
 
+import com.jcabi.manifests.Manifests;
+import com.rexsl.page.BasePage;
 import com.rexsl.page.BaseResource;
+import com.rexsl.page.Inset;
 import com.rexsl.page.Resource;
+import com.rexsl.page.inset.FlashInset;
+import com.rexsl.page.inset.LinksInset;
+import com.rexsl.page.inset.VersionInset;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * Abstract RESTful resource.
@@ -16,6 +25,36 @@ import com.rexsl.page.Resource;
  * @version $Id$
  */
 @Resource.Forwarded
+@Inset.Default({ LinksInset.class, FlashInset.class })
 public class BaseRs extends BaseResource {
+
+    /**
+     * Inset with a version of the product.
+     * @return The inset
+     */
+    @Inset.Runtime
+    public Inset ver() {
+        return new VersionInset(
+            Manifests.read("Example-Version"),
+            Manifests.read("Example-Revision"),
+            Manifests.read("Example-Date")
+        );
+    }
+
+    /**
+     * Supplementary inset.
+     * @return The inset
+     */
+    @Inset.Runtime
+    public Inset supplementary() {
+        return new Inset() {
+            @Override
+            public void render(final BasePage<?, ?> page,
+                final Response.ResponseBuilder builder) {
+                builder.type(MediaType.TEXT_XML);
+                builder.header(HttpHeaders.VARY, "Cookie");
+            }
+        };
+    }
 
 }
---
+++ rexsl-maven-archetype/src/main/resources/archetype-resources/src/main/java/EmptyPage.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2011-2013, ReXSL.com
+ * All rights reserved.
+ */
+package ${package};
+
+import com.rexsl.page.BasePage;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Base RESTful page.
+ *
+ * <p>All other JAXB pages are inherited from this class, in runtime,
+ * by means of {@link com.rexsl.page.PageBuilder}.
+ *
+ * <p>The class is mutable and NOT thread-safe.
+ *
+ * @author Yegor Bugayenko (yegor@tpc2.com)
+ * @version $Id$
+ */
+@XmlRootElement(name = "page")
+@XmlAccessorType(XmlAccessType.NONE)
+public class EmptyPage extends BasePage<EmptyPage, BaseRs> {
+
+}
