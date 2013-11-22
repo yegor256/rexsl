cfebfc056ddb329f38e2741714fb280f1ed36e54
Yegor Bugayenko <yegor@tpc2.com>
2013-11-22 12:00:24 +0100

    DEBUG log of request and response

--- rexsl-test/src/main/java/com/rexsl/test/BaseRequest.java
+++ rexsl-test/src/main/java/com/rexsl/test/BaseRequest.java
@@ -218,6 +218,14 @@ final class BaseRequest implements Request {
         final Response response = this.wire.send(
             this, this.home, this.mtd, headers, this.content
         );
+        if (Logger.isDebugEnabled(this)) {
+            Logger.debug(
+                this, "#fetch(%s %s):\nHTTP Request:\n%s\nHTTP Response:\n%s",
+                this.mtd, this.home,
+                BaseRequest.indent(this.toString()),
+                BaseRequest.indent(response.toString())
+            );
+        }
         Logger.info(
             this,
             "#fetch(%s %s): completed in %[ms]s [%d %s]: %s",
@@ -274,6 +282,17 @@ final class BaseRequest implements Request {
             }
         }
         return text.toString();
+    }
+
+    /**
+     * Indent provided text.
+     * @param text Text to indent
+     * @return Indented text
+     */
+    private static String indent(final String text) {
+        return new StringBuilder("  ")
+            .append(text.replaceAll("(\n|\n\r)", "$1  "))
+            .toString();
     }
 
     /**
