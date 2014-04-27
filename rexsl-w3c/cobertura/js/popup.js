fa30f7966dbe18c37cff3659aba4abd59280231c
Yegor Bugayenko <yegor@tpc2.com>
2014-04-27 12:18:00 +0200

    stubs for all pages in rexsl-w3c

--- rexsl-w3c/cobertura/js/popup.js
+++ rexsl-w3c/cobertura/js/popup.js
@@ -1,8 +1,11 @@
-var newwindow;
-function popupwindow(url)
-{
-	newwindow=window.open(url,'name','height=500,width=500,resizable=yes,scrollbars=yes');
-	if (window.focus) {
-		newwindow.focus()
-	}
-}
+<html>
+  <head>
+    <title>redirect</title>
+  </head>
+  <body>
+    <p>
+      Hey, glad to see you here! A few days ago 
+      rexsl-w3c module migrated to <a href="http://w3c.jcabi.com/">w3c.jcabi.com</a>.
+    </p>
+  </body>
+</html>
