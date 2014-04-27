6e8346fc2e97e0dc977702d7825296f700712ebf
Yegor Bugayenko <yegor@tpc2.com>
2014-04-27 12:16:30 +0200

    stub for all pages of rexsl-test

--- rexsl-test/cobertura/js/popup.js
+++ rexsl-test/cobertura/js/popup.js
@@ -1,8 +1,12 @@
-var newwindow;
-function popupwindow(url)
-{
-	newwindow=window.open(url,'name','height=500,width=500,resizable=yes,scrollbars=yes');
-	if (window.focus) {
-		newwindow.focus()
-	}
-}
+<html>
+  <head>
+    <title>redirect</title>
+  </head>
+  <body>
+    <p>
+      Hey, glad to see you here! A few days ago 
+      rexsl-test module migrated to <a href="http://http.jcabi.com/">http.jcabi.com</a> (fluent HTTP client)
+      and <a href="http://matchers.jcabi.com">matchers.jcabi.com</a> (XHTML matchers).
+    </p>
+  </body>
+</html>
